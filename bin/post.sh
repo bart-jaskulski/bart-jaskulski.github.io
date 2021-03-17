ea50b327b90dd8f2c36a297f0474c5b167a0dcc4
Bart Jaskulski <bjaskulski@protonmail.com>
2021-03-17 09:46:38 +0100

    Add bash script for new post generation

---
+++ bin/post.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+slugify () {
+  echo "$@" | iconv -t ascii//TRANSLIT | sed -r s/[^a-zA-Z0-9]+/-/g | sed -r s/^-+\|-+$//g | tr A-Z a-z
+}
+
+read -p "Post title: " post_title
+FILENAME="$(date +'%Y-%m-%d')-$(slugify $post_title[*])"
+cat <<EOF > ./_posts/${FILENAME}.md
+---
+layout: post
+title: "${post_title[*]}"
+date: $(date +'%Y-%m-%d %H:%M:%S %z')
+---
+EOF
+echo "New post created!"
+atom ./_posts/${FILENAME}.md
